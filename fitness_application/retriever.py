cbeebd24cda511d602532dff0050771ded5aef11
codeflamer <emryzs01@gmail.com>
2026-03-09 13:12:41 +0300

    REVELATIONS 1:1

--- fitness_application/retriever.py
+++ fitness_application/retriever.py
@@ -15,9 +15,12 @@ model_name = "BAAI/bge-large-en-v1.5"
 embeddings = FastEmbedEmbeddings(model_name=model_name, parallel=0)
 
 QDRANT_URL = os.getenv("QDRANT_URL", "http://127.0.0.1:6333")
+QDRANT_API_KEY = os.get_env("QDRANT_API_KEY", "")
 
 def load_collection(documents):
-    client = QdrantClient(url=QDRANT_URL)
+    client = QdrantClient(
+        url=QDRANT_URL
+    )
 
     collection_name = "fitness_collection_instruction"
 
